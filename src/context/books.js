26fb835025d85666b3ec0b6a7c60445a7c896135
Nik Uzair <nikuzairsc@gmail.com>
2023-08-20 07:55:14 +0800

    Add Provider to BookContext

--- src/context/books.js
+++ src/context/books.js
@@ -1,5 +1,23 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 const BooksContext = createContext();
 
+function Provider({ children }) {
+  const [count, setCount] = useState(0);
+
+  const valuetoShare = {
+    count: count,
+    incrementCount: () => {
+      setCount(count + 1);
+    },
+  };
+
+  return (
+    <BooksContext.Provider value={valuetoShare}>
+      {children}
+    </BooksContext.Provider>
+  );
+}
+
+export { Provider };
 export default BooksContext;
