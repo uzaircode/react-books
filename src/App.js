656c42cb28c07bc2e29abcfbd8aaa0dbeb8e5359
Nik Uzair <nikuzairsc@gmail.com>
2023-08-19 20:55:15 +0800

    Updating a Record

--- src/App.js
+++ src/App.js
@@ -31,10 +31,14 @@ function App() {
     console.log(updatedBooks);
   };
 
-  const editBookById = (id, newTitle) => {
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
+
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
+        return { ...book, ...response.data };
       }
 
       return book;
