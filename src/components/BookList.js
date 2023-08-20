aab988e0e67d20b04a68da1cae669de0dfc71d02
Nik Uzair <nikuzairsc@gmail.com>
2023-08-20 08:15:22 +0800

    Remove Dummy Context

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -3,21 +3,13 @@ import BooksContext from '../context/books';
 import BookShow from './BookShow';
 
 function BookList({ books, onDelete, onEdit }) {
-  const { count, incrementCount } = useContext(BooksContext);
-
   const renderedBooks = books.map((book) => {
     return (
       <BookShow onDelete={onDelete} onEdit={onEdit} key={book.id} book={book} />
     );
   });
 
-  return (
-    <div className="book-list">
-      {count}
-      <button onClick={incrementCount}>Click</button>
-      {renderedBooks}
-    </div>
-  );
+  return <div className="book-list">{renderedBooks}</div>;
 }
 
 export default BookList;
--- src/context/books.js
+++ src/context/books.js
@@ -3,20 +3,7 @@ import { createContext, useState } from 'react';
 const BooksContext = createContext();
 
 function Provider({ children }) {
-  const [count, setCount] = useState(0);
-
-  const valuetoShare = {
-    count: count,
-    incrementCount: () => {
-      setCount(count + 1);
-    },
-  };
-
-  return (
-    <BooksContext.Provider value={valuetoShare}>
-      {children}
-    </BooksContext.Provider>
-  );
+  return <BooksContext.Provider value={{}}>{children}</BooksContext.Provider>;
 }
 
 export { Provider };
